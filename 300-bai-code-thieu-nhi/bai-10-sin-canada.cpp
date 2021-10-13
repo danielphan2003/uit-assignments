6fe10efa40aeec66660fcd812be681d4b2d19177
Daniel Phan <danielphan.2003@gmail.com>
2021-10-13 16:44:37 +0700

    bai-10-sin-canada: cleaned

--- 300-bai-code-thieu-nhi/bai-10-sin-canada.cpp
+++ 300-bai-code-thieu-nhi/bai-10-sin-canada.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 
 int main() {
-  std::string s;
-  do {
+  while (1) {
+    std::string s;
     std::cout << "SIN (0 de thoat): ";
     std::cin >> s;
 
+    if (s == "0") break;
+
     int s1 = 0, s2 = 0;
 
-    for (int i = 0; i < s.length() - 1; i += 2) {
-      s1 = s1 + (int(s[i]) - int('0'));
+    for (int i = 0; i < s.length(); i += 2) {
+      if (i < s.length() - 1) s1 += (int(s[i]) - int('0'));
+      if (i > 0) s2 += (int(s[i - 1]) - int('0'))*2;
     }
-
-    for (int i = 1; i < s.length(); i += 2)
-      s2 = s2 + (int(s[i]) - int('0'))*2;
 
     if (s2 / 10 >= 1) s2 = (s2 % 10) + (s2 / 10);
 
-    if ((s1 + s2 + int(s[s.length() - 1])) % 10 != 0)
+    if ((s1 + s2 + int(s[s.length() - 1])) % 10)
       std::cout << "SIN khong hop le!\n";
     else
       std::cout << "SIN hop le!\n";
-
-  } while (s != "0");
+  };
 }
